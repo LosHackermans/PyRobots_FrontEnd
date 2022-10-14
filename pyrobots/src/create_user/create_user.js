60d4ff0ce69ad3c01737df70b5b8317d6f5475f8
Paola Sbert <paosbert@gmail.com>
2022-10-13 22:32:30 -0300

    user registration form structure added

--- pyrobots/src/create_user/create_user.js
+++ pyrobots/src/create_user/create_user.js
@@ -1,5 +1,29 @@
+
+const handleChange = () => {}
+
+const handleSubmit = () => {}
+
 function Create_user() {
-    return <h2>Create user</h2>;
+    return (
+      <>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <label>Name: </label>
+          <input type="text" name="name" placeholder="enter your name" onChange={handleChange} />
+          <br />
+          <br />
+          <label>Email: </label>
+          <input type="text" name="email" placeholder="enter your email" onChange={handleChange} />
+          <br />
+          <br />
+          <label>Password: </label>
+          <input type="password" name="password" placeholder="enter your password" onChange={handleChange} />
+          <br />
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      </>
+    );
   }
 
 export default Create_user;
