73edc6a2dbf787ae1869a38af1a0f1f1f6875641
SantiMtCo <santimonserr@hotmail.com>
2022-10-15 19:58:11 -0300

    move match files

--- pyrobots/src/create_match/create_match.js
+++
@@ -1,5 +0,0 @@
-function Create_match() {
-    return <h2>Create match</h2>;
-  }
-
-export default Create_match;
---
+++ pyrobots/src/match/createMarch.js
@@ -0,0 +1,5 @@
+function CreateMatch() {
+    return <h2>Create match</h2>;
+  }
+
+export default CreateMatch;
