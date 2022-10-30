5904c036d2e237b44d1982f1a067ae3fc3ff77c7
Federico Virgolini <fvirgolini@gmail.com>
2022-10-30 16:21:05 -0300

    list joined matches added, join match button added

--- pyrobots/src/match/ListMatches.js
+++ pyrobots/src/match/ListMatches.js
@@ -5,34 +5,106 @@ import axios from 'axios';
 function ListMatches() {
 
   const [createdMaches, setCreatedMatches] = useState([]);
+  const [joinedMatches, setJoinedMatches] = useState([]);
   const [joinableMatches, setJoinableMatches] = useState([]);
+  const [error, setError] = useState('');
+  const [robots, setRobots] = useState([]);
+  const [selectedRobot, setSelectedRobot] = useState("");
 
-  useEffect(() => {
+  const getMatches = () => {
     axios.get(`${process.env.REACT_APP_BACKEND_URL}/matches`)
+    .then(function (response) {
+      setCreatedMatches(response.data.User_Games);
+      setJoinedMatches(response.data.Games_already_join)
+      setJoinableMatches(response.data.Games_To_Join);
+    })
+    .catch(function (error) {
+      setCreatedMatches([]);
+      setJoinedMatches([])
+      setJoinableMatches([]);
+    });
+  }
+
+  const getRobots = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/robots`)
       .then(function (response) {
-        setCreatedMatches(response.data.PartidasDeUsuario);
-        setJoinableMatches(response.data.PartidasParaUnirse);
+        setRobots(response.data.robots);
       })
       .catch(function (error) {
-        setCreatedMatches([]);
-        setJoinableMatches([]);
+        setRobots([]);
       });
+  }
+
+  useEffect(() => {
+    getMatches();
+    getRobots();
   }, []);
+
+  const handleJoin = (id) => {
+    setError("");
+    if (selectedRobot === "") {
+      setError("A robot must be selected");
+      return;
+    }
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/join_match`, {
+      matchId: id,
+      robotID: selectedRobot
+    })
+      .then(function (response) {
+        if (response.data.detail) {
+          alert(response.data.detail);
+        }
+        else if (response.data.error) {
+          setError(response.data.error);
+        }
+      })
+      .catch(function (error) {
+        setError(error.message);
+      });
+  }
   
   return (
     <>
-      <h2>Matches list</h2>
+      <h2>Matches</h2>
       <div>
-        <h3>Your matches:</h3>
+        <h3>Created matches:</h3>
         <ul>
-          {createdMaches.map((element) => <li key={element.id} > {element.name} </li>)}
+          {createdMaches.map((element) =>
+          <li key={element.id} >
+            {element.name}
+            {/* <button> Details </button> */}
+          </li>)}
+        </ul>
+      </div>
+      <div>
+        <h3>Joined matches:</h3>
+        <ul>
+          {joinedMatches.map((element) =>
+          <li key={element.id} >
+            {element.name}
+            {/* <button> Details </button> */} 
+          </li>)}
         </ul>
       </div>
       <div>
         <h3>Matches to join:</h3>
+      <div>
+          <label> Select a robot to join a match: </label>
+          <select data-testid="robot-select" key="robots" name="id_robot" onChange={(event) => setSelectedRobot(event.target.value)} required>
+            <option value="">-select your robot-</option>
+            {
+              robots.map((element, index) => <option key={element.id} value={element.id}>{element.name}</option>)
+            }
+          </select>
+        </div>
         <ul>
-          {joinableMatches.map((element) => <li key={element.id} > {element.name} </li>)}
+          {joinableMatches.map((element) =>
+          <li key={element.id} >
+            {element.name}
+            <button onClick={() => handleJoin(element.id)}> Join </button>
+          </li>)}
         </ul>
+        {error && <div>{error}</div>}
       </div>
     </>
   )
---
+++ pyrobots/src/match/ListMatches.test.js
@@ -0,0 +1,17 @@
+const matchesTest = {
+  "PartidasDeUsuario": [
+    { 'id': 1, 'name': 'match1' },
+    { 'id': 2, 'name': 'match2' },
+    { 'id': 3, 'name': 'match3' }
+  ],
+  "PartidasUnidas": [
+    { 'id': 4, 'name': 'match4' },
+    { 'id': 5, 'name': 'match5' },
+    { 'id': 6, 'name': 'match6' }
+  ],
+  "PartidasParaUnirse": [
+    { 'id': 7, 'name': 'match7' },
+    { 'id': 8, 'name': 'match8' },
+    { 'id': 9, 'name': 'match9' }
+  ]
+}
