79c508559e4603d23b55891561acaedd71c3ca3b
Paola Sbert <paosbert@gmail.com>
2022-10-31 15:57:14 -0300

    drawing robots on canvas

--- pyrobots/src/simulation/GameBoard.js
+++ pyrobots/src/simulation/GameBoard.js
@@ -1,23 +1,46 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import './Simulation.css';
 
+const colors = ["red", "green", "blue", "yellow"];
+
+const drawRobots = (context, coorX, coorY, color) => {
+    context.beginPath()
+    context.arc(coorX, coorY, 5, 0, Math.PI * 2);
+    context.fillStyle = color;
+    context.fill();
+    context.closePath();
+}
+
+const draw = (context, canvas, x, y, color) => {
+    context.clearRect(x, y, canvas.width, canvas.height);
+    drawRobots(context, x, y, color);
+    // requestAnimationFrame(draw)
+}
+
 function GameBoard(props) {
+    const [canvasContext, setCanvasContext] = useState(null);
+    const canvasRef = useRef(null);
 
-    let gameBoardRef = useRef(null);
-    let contextRef = useRef(null);
-    
     useEffect(() => {
-        const gameBoard = gameBoardRef.current;
-        const context = gameBoard.getContext('2d');
-        contextRef.current = context;
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        setCanvasContext(context);
 
-    }, []);
+        if (props.data.rounds !== undefined) {
+            for (let i = 0; i < props.data.rounds.length; i++) {
+                draw(canvasContext, canvas, props.data.rounds[i].x, props.data.rounds[i].y, colors[i])
+            }
+        }
 
+    }, [canvasRef, canvasContext, props.data.rounds]);
 
     return (
-        <canvas id="GameBoard" ref={gameBoardRef}>
-        </canvas>
+        <canvas id="gameBoard" ref={canvasRef} />
     );
 }
 
 export default GameBoard;
+
+GameBoard.defaultProps = {
+    data: {},
+};
