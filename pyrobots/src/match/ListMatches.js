49bda8613d7826481113012950cc40a2474233f6
Federico Virgolini <fvirgolini@gmail.com>
2022-10-31 09:12:07 -0300

    join match code deleted

--- pyrobots/src/match/ListMatches.js
+++ pyrobots/src/match/ListMatches.js
@@ -1,15 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-
 function ListMatches() {
 
   const [createdMaches, setCreatedMatches] = useState([]);
   const [joinedMatches, setJoinedMatches] = useState([]);
   const [joinableMatches, setJoinableMatches] = useState([]);
   const [error, setError] = useState('');
-  const [robots, setRobots] = useState([]);
-  const [selectedRobot, setSelectedRobot] = useState("");
 
   const getMatches = () => {
     axios.get(`${process.env.REACT_APP_BACKEND_URL}/matches`)
@@ -22,57 +19,24 @@ function ListMatches() {
       setCreatedMatches([]);
       setJoinedMatches([])
       setJoinableMatches([]);
+      setError(error.message);
     });
-  }
-
-  const getRobots = () => {
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/robots`)
-      .then(function (response) {
-        setRobots(response.data.robots);
-      })
-      .catch(function (error) {
-        setRobots([]);
-      });
   }
 
   useEffect(() => {
     getMatches();
-    getRobots();
   }, []);
-
-  const handleJoin = (id) => {
-    setError("");
-    if (selectedRobot === "") {
-      setError("A robot must be selected");
-      return;
-    }
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/join_match`, {
-      matchId: id,
-      robotID: selectedRobot
-    })
-      .then(function (response) {
-        if (response.data.detail) {
-          alert(response.data.detail);
-        }
-        else if (response.data.error) {
-          setError(response.data.error);
-        }
-      })
-      .catch(function (error) {
-        setError(error.message);
-      });
-  }
   
   return (
     <>
       <h2>Matches</h2>
+      <button onClick={() => getMatches()} > Refresh </button>
       <div>
         <h3>Created matches:</h3>
         <ul>
           {createdMaches.map((element) =>
           <li key={element.id} >
             {element.name}
-            {/* <button> Details </button> */}
           </li>)}
         </ul>
       </div>
@@ -82,26 +46,15 @@ function ListMatches() {
           {joinedMatches.map((element) =>
           <li key={element.id} >
             {element.name}
-            {/* <button> Details </button> */} 
           </li>)}
         </ul>
       </div>
       <div>
         <h3>Matches to join:</h3>
-      <div>
-          <label> Select a robot to join a match: </label>
-          <select data-testid="robot-select" key="robots" name="id_robot" onChange={(event) => setSelectedRobot(event.target.value)} required>
-            <option value="">-select your robot-</option>
-            {
-              robots.map((element, index) => <option key={element.id} value={element.id}>{element.name}</option>)
-            }
-          </select>
-        </div>
         <ul>
           {joinableMatches.map((element) =>
           <li key={element.id} >
             {element.name}
-            <button onClick={() => handleJoin(element.id)}> Join </button>
           </li>)}
         </ul>
         {error && <div>{error}</div>}
